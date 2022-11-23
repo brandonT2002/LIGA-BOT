d429a68ca9dffe1ef0b92ee1235d92141abe4ad7
brandonT2002 <3060571900304@ingenieria.usac.edu.gt>
2022-11-22 19:51:18 -0600

    clase encuentro

---
+++ Encuentro.py
@@ -0,0 +1,8 @@
+class Encuentro:
+    def __init__(self,fecha,temporada,equipo1,equipo2,goles1,goles2):
+        self.fecha = fecha
+        self.temporada = temporada
+        self.equipo1 = equipo1
+        self.equipo2 = equipo2
+        self.goles1 = goles1
+        self.goles2 = goles2
